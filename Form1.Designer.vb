eccb580c251b1dca66f2a7bfd379ad5f81201320
patelraj09 <patelraj04434@gmai.com>
2024-01-31 18:54:23 +0530

    Uploaded

---
+++ Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        ProjectToolStripMenuItem = New ToolStripMenuItem()
+        FileToolStripMenuItem1 = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        SaveAsToolStripMenuItem = New ToolStripMenuItem()
+        EditToolStripMenuItem = New ToolStripMenuItem()
+        UndoToolStripMenuItem = New ToolStripMenuItem()
+        RedoToolStripMenuItem = New ToolStripMenuItem()
+        CopyToolStripMenuItem = New ToolStripMenuItem()
+        CutToolStripMenuItem = New ToolStripMenuItem()
+        PasteToolStripMenuItem = New ToolStripMenuItem()
+        ViewToolStripMenuItem = New ToolStripMenuItem()
+        CodeToolStripMenuItem = New ToolStripMenuItem()
+        DesginerToolStripMenuItem = New ToolStripMenuItem()
+        MenuStrip.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip
+        ' 
+        MenuStrip.BackColor = Color.Silver
+        MenuStrip.ImageScalingSize = New Size(20, 20)
+        MenuStrip.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, EditToolStripMenuItem, ViewToolStripMenuItem})
+        MenuStrip.Location = New Point(0, 0)
+        MenuStrip.Name = "MenuStrip"
+        MenuStrip.Size = New Size(800, 28)
+        MenuStrip.TabIndex = 1
+        MenuStrip.Text = "MenuStrip"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {OpenToolStripMenuItem, SaveToolStripMenuItem, SaveAsToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(46, 24)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ProjectToolStripMenuItem, FileToolStripMenuItem1})
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.O
+        OpenToolStripMenuItem.Size = New Size(229, 26)
+        OpenToolStripMenuItem.Text = "Open"
+        ' 
+        ' ProjectToolStripMenuItem
+        ' 
+        ProjectToolStripMenuItem.Name = "ProjectToolStripMenuItem"
+        ProjectToolStripMenuItem.Size = New Size(138, 26)
+        ProjectToolStripMenuItem.Text = "Project"
+        ' 
+        ' FileToolStripMenuItem1
+        ' 
+        FileToolStripMenuItem1.Name = "FileToolStripMenuItem1"
+        FileToolStripMenuItem1.Size = New Size(138, 26)
+        FileToolStripMenuItem1.Text = "File"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.S
+        SaveToolStripMenuItem.Size = New Size(229, 26)
+        SaveToolStripMenuItem.Text = "Save"
+        ' 
+        ' SaveAsToolStripMenuItem
+        ' 
+        SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        SaveAsToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.Shift Or Keys.S
+        SaveAsToolStripMenuItem.Size = New Size(229, 26)
+        SaveAsToolStripMenuItem.Text = "SaveAs"
+        ' 
+        ' EditToolStripMenuItem
+        ' 
+        EditToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {UndoToolStripMenuItem, RedoToolStripMenuItem, CopyToolStripMenuItem, CutToolStripMenuItem, PasteToolStripMenuItem})
+        EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        EditToolStripMenuItem.Size = New Size(49, 24)
+        EditToolStripMenuItem.Text = "Edit"
+        ' 
+        ' UndoToolStripMenuItem
+        ' 
+        UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        UndoToolStripMenuItem.Size = New Size(177, 26)
+        UndoToolStripMenuItem.Text = "Undo"
+        ' 
+        ' RedoToolStripMenuItem
+        ' 
+        RedoToolStripMenuItem.Name = "RedoToolStripMenuItem"
+        RedoToolStripMenuItem.Size = New Size(177, 26)
+        RedoToolStripMenuItem.Text = "Redo"
+        ' 
+        ' CopyToolStripMenuItem
+        ' 
+        CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        CopyToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.C
+        CopyToolStripMenuItem.Size = New Size(177, 26)
+        CopyToolStripMenuItem.Text = "Copy"
+        ' 
+        ' CutToolStripMenuItem
+        ' 
+        CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        CutToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.X
+        CutToolStripMenuItem.Size = New Size(177, 26)
+        CutToolStripMenuItem.Text = "Cut"
+        ' 
+        ' PasteToolStripMenuItem
+        ' 
+        PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        PasteToolStripMenuItem.ShortcutKeys = Keys.Control Or Keys.V
+        PasteToolStripMenuItem.Size = New Size(177, 26)
+        PasteToolStripMenuItem.Text = "Paste"
+        ' 
+        ' ViewToolStripMenuItem
+        ' 
+        ViewToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CodeToolStripMenuItem, DesginerToolStripMenuItem})
+        ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        ViewToolStripMenuItem.Size = New Size(55, 24)
+        ViewToolStripMenuItem.Text = "View"
+        ' 
+        ' CodeToolStripMenuItem
+        ' 
+        CodeToolStripMenuItem.Name = "CodeToolStripMenuItem"
+        CodeToolStripMenuItem.Size = New Size(151, 26)
+        CodeToolStripMenuItem.Text = "Code"
+        ' 
+        ' DesginerToolStripMenuItem
+        ' 
+        DesginerToolStripMenuItem.Name = "DesginerToolStripMenuItem"
+        DesginerToolStripMenuItem.Size = New Size(151, 26)
+        DesginerToolStripMenuItem.Text = "Desginer"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(MenuStrip)
+        FormBorderStyle = FormBorderStyle.SizableToolWindow
+        IsMdiContainer = True
+        MainMenuStrip = MenuStrip
+        Name = "Form1"
+        Text = "FromMain"
+        WindowState = FormWindowState.Maximized
+        MenuStrip.ResumeLayout(False)
+        MenuStrip.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProjectToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FileToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RedoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CodeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DesginerToolStripMenuItem As ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub CodeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CodeToolStripMenuItem.Click
+        Dim codefrm As New Code
+        codefrm.MdiParent = Me
+        codefrm.Show()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Dim codefrm As New Code
+        codefrm.MdiParent = Me
+        codefrm.Show()
+        codefrm.Label1.Text &= "Data from form1"
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim codefrm As New Code
+        codefrm.MdiParent = Me
+        codefrm.Show()
+    End Sub
+End Class
+
